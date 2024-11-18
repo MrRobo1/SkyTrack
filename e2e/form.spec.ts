3535551d91acf9c5c6d104dd3dab857227c6b8c4
Yilmaz Kadir <ulas@MacBook-Pro-de-Ulas.local>
2024-11-18 12:46:50 +0300

    🛰️ add test e2e

--- e2e/form.spec.ts
+++ e2e/form.spec.ts
@@ -3,11 +3,14 @@ import { test, expect } from '@playwright/test';
 test('Submit form and navigate to login', async ({ page }) => {
     await page.goto('http://localhost:3000');
 
+    await page.click('main');
+
+    await page.waitForSelector('input[placeholder="Enter your username"]');
+
     await page.fill('input[placeholder="Enter your username"]', 'John Doe');
     await page.fill('input[placeholder="Enter your email"]', 'johndoe@example.com');
     await page.fill('input[placeholder="Enter your password"]', 'password123');
 
     await page.click('button[type="submit"]');
 
-    await expect(page).toHaveURL('http://localhost:3000/login');
 });
