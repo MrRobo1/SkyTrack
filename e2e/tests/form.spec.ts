da4e1c3c7875dfa9df4599fe3342a2d0509f29b1
Yilmaz Kadir <ulas@MacBook-Pro-de-Ulas.local>
2024-12-01 14:49:56 +0300

    🧪 fix CI

--- e2e/tests/form.spec.ts
+++ e2e/tests/form.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Submit form and navigate to login', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('http://frontend:3000');
 
     await page.click('main');
 
--- frontend/app/api/healtcheck/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-// The endpoint will be (hostname)/api/healthcheck
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ status: "Works fine" });
-}
