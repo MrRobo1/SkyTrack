6289df598f22fce7e863d216c99022cd130dd022
Yilmaz Kadir <ulas@MacBook-Pro-de-Ulas.local>
2024-12-31 17:07:34 +0300

    ➕ add navbar from shadcnui

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/header/navbar";
+
+function dashboard() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default dashboard;
