11e82dd64ec35fc94202cab965fe814530c157e9
Yilmaz Kadir <ulas@MacBook-Pro-de-Ulas.local>
2025-02-17 15:28:44 +0300

    add icon for sign out

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+// import DashboardCard from "@/app/ui/dashboard/dashboard-card";
 
 export default async function Page() {
   const cookieStore = await cookies();
@@ -12,7 +13,7 @@ export default async function Page() {
   return (
     <div className="min-h-screen text-white">
       <div className="pt-16 container mx-auto p-4 space-y-6">
-        <h1 className="text-left text-2xl font-bold">Stat</h1>
+        {/* <DashboardCard /> */}
       </div>
 
       <div>
