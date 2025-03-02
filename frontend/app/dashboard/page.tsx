ec37c41abdb48a672c5318f2021c4adc6106448b
Yilmaz Kadir <ulas@192.168.1.105>
2025-03-02 22:42:06 +0300

    🔐 secure routes for dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,15 +1,6 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 // import DashboardCard from "@/app/ui/dashboard/dashboard-card";
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("token")?.value;
-
-  if (!token) {
-    redirect("/login");
-  }
-
   return (
     <div className="min-h-screen text-white">
       <div className="pt-16 container mx-auto p-4 space-y-6">
