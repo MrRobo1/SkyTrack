2a8d54dff018b21023903d8f80408bdab26e81c9
Yilmaz Kadir <ulas@MacBook-Pro-de-Ulas.local>
2025-04-01 11:25:50 +0200

    🔧 reorganization of the sign in and sign up page & fix e2e test

--- frontend/app/ui/dashboard/navbar.tsx
+++ frontend/app/ui/dashboard/navbar.tsx
@@ -29,7 +29,7 @@ export default function NavBar() {
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40 bg-[url('/bgSkytrack.png')] bg-center bg-cover bg-no-repeat"
-        href="/"
+        href="/dashboard"
       >
         <div className="w-32 text-white md:w-40">
           <SkytrackLogo />
